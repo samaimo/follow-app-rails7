fb57d359abd54fd33f570069cca7a42174a0863e
samaimo <kii328815@gmail.com>
2024-05-10 10:13:02 +0900

    Relationshipコントローラーを作成&createアクション

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,7 @@
+class RelationshipsController < ApplicationController
+  def create
+    follow = current_user.active_relationships.new(follower_id: params[:user_id])
+    follow.save
+    redirect_to root_path
+  end
+end
